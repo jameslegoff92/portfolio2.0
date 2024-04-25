ae67041ecaa4ccb1fdbeef1c0dc8706d0040e0c2
James Le-Goff <jameslegoff92@gmail.com>
2024-04-25 17:59:07 -0400

    Added theme persistence by using localStorage

--- client/src/app.js
+++ client/src/app.js
@@ -1,17 +1,31 @@
 import { setMobileNavControl } from "./js/mobileNavigation.js";
 import { createSketch } from "./js/p5.js";
 
-document.addEventListener("DOMContentLoaded", () => {   
-  const toggle = document.querySelector(".theme-toggle");
+document.addEventListener("DOMContentLoaded", () => {
+  let toggle = document.querySelector(".theme-toggle");
+  var theme = localStorage.getItem("theme");
+
+  if (theme === "false") {
+    toggle.checked = false
+    body.classList.replace("dark-theme", "light-theme");
+  } else if (theme === null) {
+    console.log("theme is null");
+  } else {
+    toggle.checked = true;
+    body.classList.replace("light-theme", "dark-theme");
+  }
 
   // Listen for a change on the toggle
   toggle.addEventListener("change", function () {
+    let toggleBoolean = toggle.checked;
+    localStorage.setItem("theme", toggleBoolean);
     // If the toggle is on, add the dark class to the body
-    const body = document.body;
+    theme = localStorage.getItem("theme");
+    console.log(theme);
 
-    if (body.classList.contains("dark-theme")) {
+    if (theme === "false") {
       body.classList.replace("dark-theme", "light-theme");
-    } else {
+    } else  {
       body.classList.replace("light-theme", "dark-theme");
     }
   });
