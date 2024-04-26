efbebe356826bd36e80d22701183f8617c4c858c
James Le-Goff <jameslegoff92@gmail.com>
2024-04-25 21:45:45 -0400

    fixed errors in js animation

--- client/src/js/gsap.js
+++ client/src/js/gsap.js
@@ -2,8 +2,8 @@ import gsap from "gsap";
 
 //Controls the animation for the heading and navigation fading into the UI.
 export const mainHeadingAnimation = () => {
-  let tl = gsap.timeline({ delay: 1 });
-  tl.from(".section-1 > span", { opacity: 0, duration: 0.7, stagger: 0.1 });
+  let tl = gsap.timeline({ delay: 1});
+  tl.from(".section-1 > span", { opacity: 0, duration: 0.7, stagger: 0.1,});
   tl.from(".section-2 > span", { opacity: 0, duration: 0.7, stagger: 0.1 });
   tl.from(".section-3 > span", { opacity: 0, duration: 0.7, stagger: 0.1 });
   tl.from(".section-4 > span", { opacity: 0, duration: 0.7, stagger: 0.1 });
