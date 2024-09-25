0323075d0509bacf15a48cb4e2edf3991ccc097f
James Le-Goff <jameslegoff92@gmail.com>
2024-09-25 10:58:41 -0400

    Fetch blog post data from Sanity's cloud database

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -1,3 +1,16 @@
+const client = require('../sanity');
+
 exports.getBlog = async (req, res) => {
+  const query = `*[_type == "post"] | order(publishedAt desc) {
+    title,
+    slug,
+    publishedAt,
+    mainImage,
+    "name": author->name,
+    "categories": categories[]->title
+  }`;
+  
+  const posts = await client.fetch(query);
+
   res.render('pages/blog', { script: 'blog.bundle.js', t: req.t});
 }
--- server/sanity.js
+++ server/sanity.js
@@ -3,7 +3,7 @@ const {createClient} = require('@sanity/client')
 
 const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
-  dataset: 'your-dataset-name',
+  dataset: 'production',
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2024-09-24', // use current date (YYYY-MM-DD) to target the latest API version
 })
