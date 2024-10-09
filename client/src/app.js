23d7e03b0b1201fc37fd9dc704ae6434cf66a527
James Le-Goff <jameslegoff92@gmail.com>
2024-10-09 11:05:51 -0400

    Improve and animate the internationalization UI component

--- client/src/app.js
+++ client/src/app.js
@@ -2,6 +2,7 @@ import { setMobileNavControl } from "./js/mobileNavigation.js";
 import { createSketch } from "./js/p5.js";
 import { loadThemeFromLocalStorage } from "./theme-control.js";
 import { animationScale } from "./js/gsap.js";
+import gsap from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
   let toggle = document.querySelector(".theme-toggle");
@@ -18,4 +19,24 @@ document.addEventListener("DOMContentLoaded", () => {
   new p5(createSketch("logo-canvas-mobile"));
   setMobileNavControl();
   animationScale("#toggle-input"); 
+
+  let lang = document.querySelector(".lang");
+  let langWrapper = document.querySelector(".lang-wrapper");
+  let chevronIcon = document.querySelector(".lang-container__icon");
+
+  lang.addEventListener("mouseenter", (e) => {
+    langWrapper.classList.remove("u-hidden");
+    langWrapper.classList.remove("animate-out");
+    langWrapper.classList.add("animate-in");
+    chevronIcon.classList.remove("return-counter");
+    chevronIcon.classList.add("rotate-counter");  
+  });
+
+  lang.addEventListener("mouseleave", (e) => {
+    langWrapper.classList.add("u-hidden");
+    langWrapper.classList.remove("animate-in");
+    langWrapper.classList.add("animate-out");
+    chevronIcon.classList.remove("rotate-counter");
+    chevronIcon.classList.add("return-counter");
+  });
 });
