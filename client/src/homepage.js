6947c6540c9304c7b8623fafd2da8f26803027a0
James Le-Goff <jameslegoff92@gmail.com>
2025-01-31 22:00:36 -0500

    Change background color of light-theme to white

--- client/src/homepage.js
+++ client/src/homepage.js
@@ -2,5 +2,5 @@ import { mainHeadingAnimation } from "./js/gsap.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   //This animation is removed during development to prevent the animation from playing every time the page is refreshed.
-  // mainHeadingAnimation(); 
+  mainHeadingAnimation();
 });
