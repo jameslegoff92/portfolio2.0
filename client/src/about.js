f91efb08b70c0e124a1d73be9e57c16ff7b5f5de
James Le-Goff <jameslegoff92@gmail.com>
2025-02-16 09:04:16 -0500

    Add gsap animations to about page

--- client/src/about.js
+++ client/src/about.js
@@ -3,6 +3,8 @@ import {
   navigationAnimation,
   revealAnimation,
   slideUpAnimation,
+  expandImageRevealAnimationScroll,
+  revealAnimationScroll,
 } from "./js/gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -10,5 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const mastertimeline = gsap.timeline();
   mastertimeline
     .add(slideUpAnimation(".about-h1"), 0)
-    .add(revealAnimation("#footer-wrapper"), 0);
+    .add(slideUpAnimation(".about-text"), 0)
+    .add(revealAnimation("#footer-wrapper"), 0)
+    .add(revealAnimation("#about-img-1"), 0)
+    .add(slideUpAnimation("#section-frontend"));
+
+   expandImageRevealAnimationScroll("#about-img-2");
+   revealAnimationScroll("#section-backend");
+   expandImageRevealAnimationScroll("#about-img-3");
+   revealAnimation("#section-devops");
 });
