2927e44decab944f4f6c598f4761398cb08164a9
James Le-Goff <jameslegoff92@gmail.com>
2025-02-25 15:04:15 -0500

    Dynamically change resume pdf based on language selection

--- server/controllers/about.js
+++ server/controllers/about.js
@@ -1,5 +1,6 @@
 exports.getAbout = async (req, res) => {
   const url = 'about';
   const path = req.path;
-  res.render('pages/about', { script: 'about.bundle.js', t: req.t, url, path });
+  const query = req.query.lng
+  res.render('pages/about', { script: 'about.bundle.js', t: req.t, url, path, query });
 }
